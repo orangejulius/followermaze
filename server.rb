f6355595f7314dd52c38f655c08978e18ab9c6b6
Julian Simioni <julian.simioni@gmail.com>
2014-11-13 16:46:11 +0100

    Use the SocketHandler in the server
    Just to sanity check everything works the same in the real world

--- server.rb
+++ server.rb
@@ -1,9 +1,11 @@
 require 'socket'
 
+require_relative 'socket_handler'
+
 server = TCPServer.new 2000
 loop do
   Thread.start(server.accept) do |socket|
-    socket.puts "Hi"
-    socket.close
+    handler = SocketHandler.new(socket)
+    handler.run
   end
 end
