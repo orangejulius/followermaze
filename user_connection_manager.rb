bbff73e2b0f659eee489c5f769882bda05e6779c
Julian Simioni <julian.simioni@gmail.com>
2014-11-17 12:06:41 +0100

    Refactor mock object code

--- user_connection_manager.rb
+++ user_connection_manager.rb
@@ -1,13 +1,19 @@
+class UserConnection
+  def initialize(socket_connection)
+  end
+end
+
 class UserConnectionManager
-  def initialize(socket, connection_limit)
+  def initialize(socket, connection_limit, user_connection_class = UserConnection)
     @socket = socket
     @connection_limit = connection_limit
+    @user_connection_class = user_connection_class
   end
 
   def run
     connections = 0
     while connections < @connection_limit
-      UserConnection.new(@socket.accept)
+      @user_connection_class.new(@socket.accept)
       connections += 1
     end
   end
--- user_connection_manager_test.rb
+++ user_connection_manager_test.rb
@@ -3,53 +3,58 @@ require 'minitest/autorun'
 
 require_relative 'user_connection_manager'
 
-describe UserConnectionManager do
-  class UserConnection
-    # mock version of the class that checks socket connection was passed successfully
-    include Minitest::Assertions
-    @@creation_count = 0
+class FakeSocket
+  def accept
+    "this would actually be a socket connection"
+  end
+end
 
-    def initialize(socket_connection)
-      assert_equal "this would actually be a socket connection", socket_connection
-      @@creation_count += 1
-    end
-
-    def self.creation_count
-      @@creation_count
-    end
+class ConnectionCountingFakeSocket < FakeSocket
+  @@accept_calls = 0
+  def self.accept_calls
+    @@accept_calls
   end
 
+  def accept
+    @@accept_calls += 1
+    super
+  end
+end
+
+class SocketCheckingUserConnection < UserConnection
+  include Minitest::Assertions
+
+  @@creation_count = 0
+
+  def initialize(socket_connection)
+    assert_equal "this would actually be a socket connection", socket_connection
+    @@creation_count += 1
+  end
+
+  def self.creation_count
+    @@creation_count
+  end
+end
+
+describe UserConnectionManager do
   it 'passes socket connection given by accept to UserConnection class' do
     fake_socket = Minitest::Mock.new
     fake_socket.expect(:accept, "this would actually be a socket connection")
 
-    manager = UserConnectionManager.new(fake_socket, 1)
+    manager = UserConnectionManager.new(fake_socket, 1, SocketCheckingUserConnection)
     manager.run
 
     fake_socket.verify
-    assert_equal 1, UserConnection.creation_count
-  end
-
-  class FakeSocket
-    @@accept_calls = 0
-
-    def accept
-      @@accept_calls += 1
-      "this would actually be a socket connection"
-    end
-
-    def self.accept_calls
-      @@accept_calls
-    end
+    assert_equal 1, SocketCheckingUserConnection.creation_count
   end
 
   it 'calls accept up to connection_limit times' do
-    fake_socket = FakeSocket.new
+    fake_socket = ConnectionCountingFakeSocket.new
 
     connection_limit = 5
-    manager = UserConnectionManager.new(fake_socket, connection_limit)
+    manager = UserConnectionManager.new(fake_socket, connection_limit )
     manager.run
 
-    assert_equal connection_limit, FakeSocket.accept_calls
+    assert_equal connection_limit, ConnectionCountingFakeSocket.accept_calls
   end
 end
