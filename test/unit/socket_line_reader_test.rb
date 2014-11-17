39838b3de20dadcb3d64bd31dd65a01034171940
Julian Simioni <julian.simioni@gmail.com>
2014-11-17 18:19:52 +0100

    Use directory specifically for unit tests

--- test/unit/socket_line_reader_test.rb
+++ test/unit/socket_line_reader_test.rb
@@ -1,8 +1,7 @@
 require 'minitest/spec'
-require 'minitest/autorun'
 require 'stringio'
 
-require_relative '../src/socket_line_reader'
+require_relative '../../src/socket_line_reader'
 
 class LineAccumulator
   attr_reader :lines
