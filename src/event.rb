52887274809012111a347def370865805727f35b
Julian Simioni <julian.simioni@gmail.com>
2014-11-18 16:48:08 +0100

    Add payload attribute to Event

--- src/event.rb
+++ src/event.rb
@@ -3,11 +3,13 @@ class Event
   attr_reader :type
   attr_reader :from
   attr_reader :to
+  attr_reader :payload
 
-  def initialize(sequence: nil, type: nil, from: nil, to: nil)
+  def initialize(sequence: nil, type: nil, from: nil, to: nil, payload: nil)
     @sequence = sequence
     @type = type
     @from = from
     @to = to
+    @payload = payload
   end
 end
