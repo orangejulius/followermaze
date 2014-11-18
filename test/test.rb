0f6b44f3921ef5b50ef01a22153a3b03a3ec8603
Julian Simioni <julian.simioni@gmail.com>
2014-11-18 13:41:05 +0100

    Add incomplete UserCollector from spike

--- test/test.rb
+++ test/test.rb
@@ -2,6 +2,7 @@ require_relative 'unit/socket_line_reader_test'
 require_relative 'unit/event_decoder_test'
 require_relative 'unit/sequencer_test'
 require_relative 'unit/user_database_test'
+require_relative 'unit/user_collector_test'
 
 require_relative 'integration/socket_line_reader_test'
 
---
+++ test/unit/user_collector_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/spec'
+
+require_relative '../mocks/accumulator'
+require_relative '../../src/user_collector'
+
+describe UserCollector do
+  describe 'when passed event with no to/from users' do
+    def setup
+      @destination = EventAccumulator.new
+      @database = Minitest::Mock.new
+      @collector = UserCollector.new(@destination, @database)
+      @event = Event.new(type: :broadcast, sequence: 1)
+
+      @collector.send_event(@event)
+    end
+
+    it 'passes event to destination' do
+      assert_equal [@event], @destination.items
+    end
+
+    it 'adds no users to UserDatabase' do
+      @database.verify
+    end
+  end
+
+  describe 'when passed event with to user' do
+    def setup
+      @database = Minitest::Mock.new
+      @destination = EventAccumulator.new
+      @event = Event.new(type: :follow, to: 1, sequence: 1)
+      @collector = UserCollector.new(@destination, @database)
+
+      @database.expect(:add, nil, [@event.to])
+      @collector.send_event(@event)
+    end
+
+    it 'calls add on user database' do
+      @database.verify
+    end
+
+    it 'passes event to destination' do
+      assert_equal [@event], @destination.items
+    end
+  end
+end
