a3ed76ad79bc1956af617507a012d65a04299b3e
Julian Simioni <julian.simioni@gmail.com>
2014-11-18 17:34:24 +0100

    Run the client connection accept loop in a separate thread
    One of the two ends of the pipeline have to be in a separate thread,
    otherwise we can't accept connections until data has filtered all the
    way through.

--- src/user_connection_manager.rb
+++ src/user_connection_manager.rb
@@ -12,11 +12,13 @@ class UserConnectionManager
 
   def run
     connections = 0
-    while connections < @connection_limit
-      connections += 1
-      @executor.start(@socket.accept) do |socket_connection|
-        connection = @user_connection_class.new socket_connection
-        @connections[connection.get_id] = connection
+    @executor.start do
+      while connections < @connection_limit
+        connections += 1
+        @executor.start(@socket.accept) do |socket_connection|
+          connection = @user_connection_class.new socket_connection
+          @connections[connection.get_id] = connection
+        end
       end
     end
   end
