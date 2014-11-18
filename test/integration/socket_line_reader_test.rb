c788f594e2d67d117ff281a74703c77a4e898e9a
Julian Simioni <julian.simioni@gmail.com>
2014-11-18 11:10:20 +0100

    Use absolute path for socket file
    This should prevent the tests from failing if not run from the correct
    directory

--- test/integration/socket_line_reader_test.rb
+++ test/integration/socket_line_reader_test.rb
@@ -4,9 +4,15 @@ require 'fileutils'
 
 describe "socket programs" do
   it 'successfully sends some data over a real socket!' do
-    FileUtils.rm_f("socket_file")
-    output = `ruby test/bin/socket_printer.rb socket_file & sleep 0.01;ruby test/bin/socket_client.rb socket_file`
+    socket_path = File.join(File.dirname(__FILE__), '../socket_file')
+
+    # clean up from previous test runs
+    FileUtils.rm_f socket_path
+
+    output = `ruby test/bin/socket_printer.rb #{socket_path} & sleep 0.01;ruby test/bin/socket_client.rb #{socket_path}`
     assert_equal "hello\nhello2\n", output
-    FileUtils.rm_f("socket_file")
+
+    # be nice to future test runs
+    FileUtils.rm_f socket_path
   end
 end
