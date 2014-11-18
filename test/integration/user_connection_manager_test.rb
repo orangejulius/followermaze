8e3c9a46b361064643b9db9ddf770f6d1b4a7680
Julian Simioni <julian.simioni@gmail.com>
2014-11-18 18:03:32 +0100

    Bump up timeout for integration test slightly
    This is probably too fragile :/

--- test/integration/user_connection_manager_test.rb
+++ test/integration/user_connection_manager_test.rb
@@ -1,6 +1,6 @@
 describe 'user connection manager' do
   it 'sends hello#{id} to each of the two expected clients' do
-    output = `ruby test/bin/static_messager.rb& sleep 0.01; ruby test/bin/static_message_client.rb 1& ruby test/bin/static_message_client.rb 2`
+    output = `ruby test/bin/static_messager.rb& sleep 0.05; ruby test/bin/static_message_client.rb 1& ruby test/bin/static_message_client.rb 2`
 
     assert_equal "ok\nok\n", output
   end
